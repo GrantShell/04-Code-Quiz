9a250f8306621b11c953d82b85bf53064a2b26c2
Grant Shell <grantshell33@icloud.com>
2021-04-10 15:25:25 -0500

    rotating questions and listener on answer buttons

--- assets/script.js
+++ assets/script.js
@@ -34,9 +34,11 @@ var questionContainerEl = document.getElementById("questions-container");
 var quizTimer = document.getElementById("timer");
 var timeLeft = 100;
 var timerInterval;
+var currentQuestion = 0;
 
 startButton.addEventListener("click", startGame);
 
+//start of game
 function startGame() {
   console.log("started");
   startButton.classList.add("hide");
@@ -51,6 +53,51 @@ function startGame() {
   }, 1000);
 }
 
-function nextQuestion() {
-  showQuestions(questions);
+function showQuestions(questionIndex) {
+  //call questions
+  //question needs to show
+  document.getElementById("answer-btns").innerHTML = "";
+  var title = questions[questionIndex].question;
+
+  document.getElementById("questions").innerHTML = title;
+
+  for (var i = 0; i < questions[questionIndex].answers.length; i++) {
+    var answer = questions[questionIndex].answers[i].text;
+    var isCorrect = questions[questionIndex].answers[i].correct;
+
+    var button = document.createElement("button");
+    button.classList.add("options");
+    button.setAttribute("correct", isCorrect);
+
+    button.innerHTML = answer;
+    document.getElementById("answer-btns").append(button);
+  }
+
+  var selectAllButtons = document.querySelectorAll(".options");
+
+  for (var i = 0; i < selectAllButtons.length; i++) {
+    selectAllButtons[i].addEventListener("click", function () {
+      currentQuestion++;
+
+      showQuestions(currentQuestion);
+    });
+  }
+
+  // answers need to show
+  // marker(not loop) ++cou
 }
+showQuestions(currentQuestion);
+// track the answers
+//connect score and timer
+
+//end game,
+//timer countdown ,wrong answers minus time with wrong answer, what does it display
+
+//scoreboard- show high score with initials
+//save to localstorage
+
+// function nextQuestion() {
+//   showQuestions(0);
+// }
+
+//timer
